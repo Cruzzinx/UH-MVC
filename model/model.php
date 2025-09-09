80982483baf3e6cd336d55ce342bbf76a6fd44d0
Hendri <hendri23072009@gmail.com>
2025-09-09 07:59:15 +0700

    model jadi

--- model/model.php
+++ model/model.php
@@ -5,7 +5,30 @@ class ArtModel extends Connection{
     protected $type;
     protected $year;
 
+    protected function FindAll(){
+        $sql = "SELECT * FROM seni";
+        $result = $this->connect()->query($sql);
+        if($result->num_rows > 0){
+            while ($data = mysqli_fetch_assoc($result)){
+                $art[] = $data;
+            }
+        }
 
+        return $art;
+    }
+
+
+    protected function FindOne($id){
+        $sql = "SELECT * FROM SENI WHERE id = " . $id;
+        $result = $this->connect()->query($sql);
+        if($result->num_rows > 0){
+            while ($data = mysqli_fetch_assoc($result)){
+                $art[] = $data;
+            }
+        }
+
+        return $art;
+    }
 }
 
 ?>
